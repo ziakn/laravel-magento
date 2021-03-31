07e402ab5ea7a91810d4aa6b94b2c79c5773c290
ziakn <ziakn03@gmail.com>
2021-03-31 11:17:00 +0300

    New Migration

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -1,103 +1,58 @@
  <div id="sidebar" class="active">
-            <div class="sidebar-wrapper active">
-                <div class="sidebar-header">
-                    <div class="d-flex justify-content-between">
-                        <div class="logo">
-                            {{-- <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a> --}}
-                             <p>{{ auth()->user()->name }}</p>
-                        </div>
-                        <div class="toggler">
-                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="sidebar-menu">
-                    <ul class="menu">
-                        <li class="sidebar-title">Menu</li>
+     <div class="sidebar-wrapper active">
+         <div class="sidebar-header">
+             <div class="d-flex justify-content-between">
+                 <div class="logo">
+                     {{-- <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a> --}}
+                     <p>{{ auth()->user()->name }}</p>
+                 </div>
+                 <div class="toggler">
+                     <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                 </div>
+             </div>
+         </div>
+         <div class="sidebar-menu">
+             <ul class="menu">
+                 <li class="sidebar-title">Menu</li>
 
-                        <li class="sidebar-item active ">
-                            <a href="index.html" class='sidebar-link'>
-                                <i class="bi bi-grid-fill"></i>
-                                <span>Dashboard</span>
-                            </a>
-                        </li>
+                 <li class="sidebar-item active ">
+                     <a href="index.html" class='sidebar-link'>
+                         <i class="bi bi-grid-fill"></i>
+                         <span>Dashboard</span>
+                     </a>
+                 </li>
 
 
-                        <li class="sidebar-title">Forms &amp; Tables</li>
+                 <li class="sidebar-title">Forms &amp; Tables</li>
 
-                        <li class="sidebar-item  has-sub">
-                            <a href="#" class='sidebar-link'>
-                                <i class="bi bi-hexagon-fill"></i>
-                                <span>Form Elements</span>
-                            </a>
-                            <ul class="submenu ">
-                                <li class="submenu-item ">
-                                    <a href="form-element-input.html">Input</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-element-input-group.html">Input Group</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-element-select.html">Select</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-element-radio.html">Radio</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-element-checkbox.html">Checkbox</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-element-textarea.html">Textarea</a>
-                                </li>
-                            </ul>
-                        </li>
 
-                        <li class="sidebar-item  ">
-                            <a href="form-layout.html" class='sidebar-link'>
-                                <i class="bi bi-file-earmark-medical-fill"></i>
-                                <span>Form Layout</span>
-                            </a>
-                        </li>
+                 <li class="sidebar-item  ">
+                     <a href="form-layout.html" class='sidebar-link'>
+                         <i class="bi bi-file-earmark-medical-fill"></i>
+                         <span>Add Date</span>
+                     </a>
+                 </li>
 
-                        <li class="sidebar-item  has-sub">
-                            <a href="#" class='sidebar-link'>
-                                <i class="bi bi-pen-fill"></i>
-                                <span>Form Editor</span>
-                            </a>
-                            <ul class="submenu ">
-                                <li class="submenu-item ">
-                                    <a href="form-editor-quill.html">Quill</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-editor-ckeditor.html">CKEditor</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-editor-summernote.html">Summernote</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="form-editor-tinymce.html">TinyMCE</a>
-                                </li>
-                            </ul>
-                        </li>
 
-                        <li class="sidebar-item  ">
-                            <a href="table.html" class='sidebar-link'>
-                                <i class="bi bi-grid-1x2-fill"></i>
-                                <span>Table</span>
-                            </a>
-                        </li>
 
-                        <li class="sidebar-item  ">
-                            <a href="table-datatable.html" class='sidebar-link'>
-                                <i class="bi bi-file-earmark-spreadsheet-fill"></i>
-                                <span>Datatable</span>
-                            </a>
-                        </li>
+                 <li class="sidebar-item  ">
+                     <a href="table.html" class='sidebar-link'>
+                         <i class="bi bi-grid-1x2-fill"></i>
+                         <span>List Data</span>
+                     </a>
+                 </li>
 
-                     
 
-                    </ul>
-                </div>
-                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
-            </div>
-        </div>
+                 <li class="sidebar-item  ">
+                     <a href="table.html" class='sidebar-link'>
+                         <i class="bi bi-grid-1x2-fill"></i>
+                         <span>Log Out</span>
+                     </a>
+                 </li>
+
+
+             </ul>
+         </div>
+         <button class="sidebar-toggler btn x"><i data-feather="x"> </i></button>
+     </div>
+ </div>
