59f032c6d67a57c342717f745ad368256ba7f8ce
ziakn <ziakn03@gmail.com>
2021-03-31 10:22:21 +0300

    Side And Header Component Completd

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,24 @@
+<header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
+<div class="page-heading">
+    {{-- <h3>Profile Statistics</h3> --}}
+    <div class="page-title">
+                    <div class="row">
+                        <div class="col-12 col-md-6 order-md-1 order-last">
+                            <h3>Alert</h3>
+                            <p class="text-subtitle text-muted">A pretty helpful component for give message to user</p>
+                        </div>
+                        <div class="col-12 col-md-6 order-md-2 order-first">
+                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Alert</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+</div>
